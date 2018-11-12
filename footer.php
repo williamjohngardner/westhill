036b377ec047c45c74cb224d8b4fe7b507dde9f9
williamjohngardner <bill@williamjohngardner.com>
2018-11-12 11:56:24 -0500

    site working on GoDaddy subdomain

--- footer.php
+++ footer.php
@@ -1,23 +1,29 @@
-    <div>
+    <div class="container footer-contact">
       <a name="contact"></a>
-      <h3>Dogwood Digital</h3>
-      <h5>5816 Hideaway Drive</h5>
-      <h5>Chapel Hill, NC </h5>
-      <h5>info@dogwooddigitalgroup.com</h5>
-      <form action="#">
-        <h3>Contact Us</h3>
-        <input type="text" placeholder="Name">
-        <input type="email" placeholder="Email">
-        <input type="text" placeholder="Phone">
-        <textarea name="footerContact" id="" cols="30" rows="10" placeholder="How Can We Help?"></textarea>
-      </form>
-      <div name="googleMap">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3227.926157920496!2d-79.07646988486803!3d35.99767082015971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89acdc48eae16953%3A0x5c0162e387b782dc!2s5816+Hideaway+Dr%2C+Chapel+Hill%2C+NC+27516!5e0!3m2!1sen!2sus!4v1541954606455" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+      <div>
+        <h3>Dogwood Digital</h3>
+        <h5 class="contact-info">5816 Hideaway Drive</h5>
+        <h5 class="contact-info">Chapel Hill, NC </h5>
+        <a href="mailto:info@dogwooddigitalgroup.com"><h5 class="contact-info contact-email">info@dogwooddigitalgroup.com</h5></a>
+      </div>
+      <div>
+        <form action="#">
+          <h3>Contact Us</h3>
+          <input type="text" placeholder="Name">
+          <input type="email" placeholder="Email">
+          <input type="text" placeholder="Phone">
+          <textarea name="footerContact" id="" cols="30" rows="8" placeholder="How Can We Help?"></textarea>
+        </form>
+      </div>
+      <div>
+        <div name="googleMap">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3227.926157920496!2d-79.07646988486803!3d35.99767082015971!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89acdc48eae16953%3A0x5c0162e387b782dc!2s5816+Hideaway+Dr%2C+Chapel+Hill%2C+NC+27516!5e0!3m2!1sen!2sus!4v1541954606455" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
       </div>
     </div>
     <footer>
-        <h5>&copy;<?php echo date('Y'); ?> | <a href="http://www.dogwooddigitalgroup.com">Dogwood Digital Group</a></h5>
+      <h5 class="footer-link">&copy;<?php echo date('Y'); ?> | <a href="http://www.dogwooddigitalgroup.com">Dogwood Digital Group</a></h5>
     </footer>
-        <?php wp_footer(); ?>
+      <?php wp_footer(); ?>
   </body>
 </html>
--- index.php
+++ index.php
@@ -1,39 +1,61 @@
 <?php get_header(); ?>
 
-    <div name="hero">
+    <div name="hero" class="services">
+        <h1 class="services-text">WE DELIVER ROI TO OUR CLIENTS</h1>
         <img src="<?php bloginfo('template_directory'); ?>/images/hero_preview.png" alt="Dogwood Digital">
     </div>
-    <div name="introduction">
+    <div name="introduction" class="intro">
         <a name="about"></a>
-        <h2>Here is the div Title</h2>
-        <h5>Here is description text</h5>
-        <img src="#" alt="#">
-        <h5>Description Text for Intro Image 1</h5>
-        <img src="#" alt="#">
-        <h5>Description Text for Intro Image 2</h5>
-        <img src="#" alt="#">
-        <h5>Description Text for Intro Image 3</h5>
+        <div class="section-titles">
+            <h2 style="display: inline-block;">WE DELIVER YOUR COMPANY SEARCH RESULTS</h2>
+        </div>
+        <div class="section-text">
+            <h5>Lorem ipsum dolor amet trust fund squid locavore seitan kale chips shaman. Woke PBR&B literally, roof party letterpress affogato ugh shoreditch sriracha ramps irony. Tofu direct trade poutine, four dollar toast raw denim copper mug migas. Echo park whatever succulents coloring book blue bottle. Microdosing af occupy craft beer umami dreamcatcher blog cronut XOXO 8-bit etsy wolf.
+                <br>
+                <br>
+                Keytar church-key shaman cold-pressed austin single-origin coffee master cleanse migas gentrify. Hashtag VHS la croix, organic food truck adaptogen palo santo ramps taxidermy actually. Forage shabby chic ennui viral pitchfork crucifix etsy direct trade photo booth pork belly live-edge four loko irony man braid tattooed. Gluten-free flexitarian church-key, snackwave mlkshk quinoa tumblr ethical seitan affogato. Skateboard flexitarian forage kitsch. Kitsch health goth prism keffiyeh gluten-free synth forage poutine live-edge whatever taiyaki hot chicken.
+                <br>
+                <br>
+                Portland gentrify af snackwave, activated charcoal pinterest synth hella cred ugh. Butcher chia taiyaki scenester man bun. Selfies drinking vinegar etsy fanny pack try-hard VHS chillwave wolf hoodie meh truffaut tilde. Narwhal truffaut gochujang health goth, palo santo listicle vexillologist drinking vinegar photo booth deep v tofu butcher +1 forage. Health goth pug fanny pack, austin ennui occupy blog. Succulents meditation blue bottle hammock, chartreuse seitan palo santo selfies selvage tilde. Raclette mustache mumblecore live-edge VHS gluten-free knausgaard retro pork belly.
+            </h5>
+        </div>
+        <div class="container">
+            <div>
+                <img src="<?php bloginfo('template_directory'); ?>/images/results_pic_mobile.png" alt="#">
+                <h5 class="section-detail-text">Lorem ipsum dolor amet trust fund squid locavore seitan kale chips shaman. Woke PBR&B literally, roof party letterpress affogato ugh shoreditch sriracha ramps irony. Tofu direct trade poutine, four dollar toast raw denim copper mug migas.</h5>
+            </div>
+            <div>
+                <img src="<?php bloginfo('template_directory'); ?>/images/results_pic_mobile.png" alt="#">
+                <h5 class="section-detail-text">Lorem ipsum dolor amet trust fund squid locavore seitan kale chips shaman. Woke PBR&B literally, roof party letterpress affogato ugh shoreditch sriracha ramps irony. Tofu direct trade poutine, four dollar toast raw denim copper mug migas.</h5>    
+            </div>
+            <div>
+                <img src="<?php bloginfo('template_directory'); ?>/images/results_pic_mobile.png" alt="#">
+                <h5 class="section-detail-text">Lorem ipsum dolor amet trust fund squid locavore seitan kale chips shaman. Woke PBR&B literally, roof party letterpress affogato ugh shoreditch sriracha ramps irony. Tofu direct trade poutine, four dollar toast raw denim copper mug migas.</h5>
+            </div>
+        </div>
     </div>
-    <div name="CTA-1">
-        <h2>Call To Action 1</h2>
-        <button name="cta">Contact Us</button>
+    <div name="CTA-1" style="border-top: 2px #333 solid; border-bottom: 2px #333 solid; height: 150px;">
+        <h2 style="display: inline; margin-left: 50px; margin-top: 50px;" class="cta-title">Call To Action 1 -  Call Us Now!!!</h2>
+        <button style="display: inline;" name="cta" class="cta sitewide-cta">Contact Us</button>
     </div>
-    <a name="services"></a>
-    <div name="service-1">
-        <h3>Service One - What's Good</h3>
-        <img src="#" alt="">
-    </div>
-    <div name="service-2">
-        <h3>Service Two - What's Cool</h3>
-        <img src="#" alt="">
-    </div>
-    <div name="service-3">
-        <h3>Service Three - What's New</h3>
-        <img src="#" alt="">
-    </div>
-    <div name="CTA-2">
-        <h2>Call To Action 2</h2>
-        <button name="cta">Contact Us</button>
+    <div>
+        <a name="services"></a>
+        <div name="service-1" class="services">
+            <h2 class="services-text">Service One - What's Good</h2>
+            <img src="<?php bloginfo('template_directory'); ?>/images/team_shot.png" alt="">
+        </div>
+        <div name="service-2" class="services">
+            <h2 class="services-text">Service Two - What's Cool</h2>
+            <img src="<?php bloginfo('template_directory'); ?>/images/team_shot.png" alt="">
+        </div>
+        <div name="service-3" class="services">
+            <h2 class="services-text">Service Three - What's New</h2>
+            <img src="<?php bloginfo('template_directory'); ?>/images/team_shot.png" alt="">
+        </div>
+    </div>    
+    <div name="CTA-2" style="border-top: 2px #333 solid; border-bottom: 2px #333 solid; height:150px;">
+        <h2 style="display: inline;" class="cta-title">Call To Action 2 - Call Us Now!!!</h2>
+        <button style="display: inline;" name="cta" class="cta sitewide-cta">Contact Us</button>
     </div>
 
 <?php get_footer(); ?>
